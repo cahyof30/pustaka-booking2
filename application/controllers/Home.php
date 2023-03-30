3a12f600173f299755ebe43a82ae79346d4b5436
cahyof30 <f30.cahyo@gmail.com>
2023-03-30 11:15:51 +0700

    Update Pertemuan 3

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -22,11 +22,13 @@ class Home extends CI_Controller
 
             $this->load->view('templates/templates-user/header', $data);
             $this->load->view('buku/daftarbuku', $data);
+            $this->load->view('templates/templates-user/modal');
             $this->load->view('templates/templates-user/footer', $data);
         } else {
             $data['user'] = 'Pengunjung';
             $this->load->view('templates/templates-user/header', $data);
             $this->load->view('buku/daftarbuku', $data);
+            $this->load->view('templates/templates-user/modal');
             $this->load->view('templates/templates-user/footer', $data);
         }
     }
