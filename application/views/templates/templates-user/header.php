57885e555854156e0ff729a07d23c84f34c3f6a6
cahyof30 <f30.cahyo@gmail.com>
2023-04-13 09:02:25 +0700

    Update Pertemuan 5

--- application/views/templates/templates-user/header.php
+++ application/views/templates/templates-user/header.php
@@ -27,6 +27,7 @@
                     if (!empty($this->session->userdata('email'))) { ?>
                         <!-- <a class="nav-item nav-link" href="#">Booking Buku</a> -->
                         <a class="nav-item nav-link" href="<?= base_url('booking'); ?>">Booking <b><?= $this->ModelBooking->getDataWhere('temp', ['email_user' => $this->session->userdata('email')])->num_rows(); ?></b> Buku</a>
+                        <a class="nav-item nav-link" onclick="information('Waktu Pengambilan Buku 1x24 jam dari Booking!!!')" href="<?php echo base_url() . 'booking/exportToPdf/' . $this->session->userdata('id_user'); ?>">Cetak Bukti Booking</a>
                         <a class="nav-item nav-link" href="<?= base_url('member/myprofil'); ?>">Profil Saya</a>
                         <a class="nav-item nav-link" href="<?= base_url('member/logout'); ?>"><i class="fas fw fa-login"></i> Log out</a>
 
@@ -35,6 +36,7 @@
                         <a class="nav-item nav-link" data-toggle="modal" data-target="#daftarModal" href="#"><i class="fas fw fa-login"></i> Daftar</a>
 
                         <a class="nav-item nav-link" data-toggle="modal" data-target="#loginModal" href="#"><i class="fas fw fa-login"></i> Log in</a>
+
 
                     <?php } ?>
                     <span class="nav-item nav-link nav-right" style="display:block; margin-left:20px;">Selamat Datang <b><?= $user; ?></b></span>
